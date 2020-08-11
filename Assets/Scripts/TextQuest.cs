dce667f219ca719ec77d2c9fd176017dd3485949
Rishab Parmar <parmar.rishab@gmail.com>
2020-08-11 17:13:52 +0530

    Pushing the completed first version of the game

---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuest : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = StartingState;
+        textComponent.text = state.GetStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = state.goToNextState()[0];
+            textComponent.text = state.GetStoryText();
+        } else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = state.goToNextState()[1];
+            textComponent.text = state.GetStoryText();
+        }
+    }
+}
